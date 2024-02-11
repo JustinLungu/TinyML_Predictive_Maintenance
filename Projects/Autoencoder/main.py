2ec89e729b647639a75197e495506fad3cf7113d
JustinLungu <iustinl113@gmail.com>
2024-02-11 18:22:14 +0100

    saving preprocessed data in csv/json

--- Projects/Autoencoder/main.py
+++ Projects/Autoencoder/main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import sys
 from data_preprocessing import Data, Preprocessing
+import pandas as pd
+import json
+import os
 
 TRAIN_RATIO = 0.7
 VAL_RATIO = 0.2
@@ -36,6 +39,33 @@ def normalization(normal_data: Data, abnormal_data: Data):
     abnormal_data.dataset = abnormal.reshape(-1, WINDOW_SIZE, DATA_SHAPE)
 
 
+def save_to_csv(data, filename):
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data.tolist(), f)
+
+def save_data(normal_data: Data, abnormal_data: Data, folder_path):
+    # Create the folder if it doesn't exist
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    # Save normal data
+    save_to_csv(normal_data.train_data.reshape(-1, 3), os.path.join(folder_path, 'normal_train.csv'))
+    save_to_csv(normal_data.val_data.reshape(-1, 3), os.path.join(folder_path, 'normal_val.csv'))
+    save_to_csv(normal_data.test_data.reshape(-1, 3), os.path.join(folder_path, 'normal_test.csv'))
+    # Save normal data in JSON format
+    save_to_json(normal_data.train_data, os.path.join(folder_path, 'normal_train.json'))
+    save_to_json(normal_data.val_data, os.path.join(folder_path, 'normal_val.json'))
+    save_to_json(normal_data.test_data, os.path.join(folder_path, 'normal_test.json'))
+
+    # Save abnormal data
+    save_to_csv(abnormal_data.dataset.reshape(-1, 3), os.path.join(folder_path, 'abnormal.csv'))
+    # Save abnormal data in JSON format
+    save_to_json(abnormal_data.dataset, os.path.join(folder_path, 'abnormal.json'))
+
 if __name__ == "__main__":
     #set the threshhold of prinitng data to console to maximum value
     #so avoid the loss of data on console while displaying
@@ -54,3 +84,6 @@ if __name__ == "__main__":
     abnormal_data.dataset = abnormal_data.make_windows(abnormal_data.dataset, WINDOW_SIZE)
     
     normalization(normal_data, abnormal_data)
+
+    # Save the preprocessed data in the specified folder
+    save_data(normal_data, abnormal_data, "Projects/Autoencoder/Preprocessed Data")
