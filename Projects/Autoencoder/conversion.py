5fd9c085317737242b2fda424f7a0015052cd037
JustinLungu <iustinl113@gmail.com>
2024-04-17 20:46:53 +0200

    inference on 3x1 data (tflite)

--- Projects/Autoencoder/conversion.py
+++ Projects/Autoencoder/conversion.py
@@ -49,7 +49,7 @@ with open(join(MODELS_FOLDER_PATH, MODEL_NAME) + '.h', 'w') as file:
 
 ###################################### TEST INFERENCE #######################################
 
-
+'''
 data_60hz30vol_normalized = [0.5075, 0.4825, 0.675 , 0.475 , 0.51 , 0.8 , 
                              0.5075, 0.4825, 0.6775, 0.475 , 0.51 , 0.8 , 
                              0.5075, 0.4825, 0.6775, 0.4775, 0.51 , 0.8 , 
@@ -62,13 +62,41 @@ data_60hz30vol_normalized = [0.5075, 0.4825, 0.675 , 0.475 , 0.51 , 0.8 ,
                              0.505 , 0.4825, 0.6875, 0.48 , 0.5075, 0.795 , 
                              0.505 , 0.4825, 0.69 , 0.48 , 0.5075, 0.795 , 
                              0.505 , 0.485 , 0.6925, 0.48 , 0.5075, 0.795]
+'''
+data_60hz30vol_normalized = [0.5075, 0.4825, 0.675]
 input_data_test = np.array(data_60hz30vol_normalized)
 
 print(input_data_test.shape)
 
-# I will copy the code from google colab used to run inference as before
+##### I will copy the code from google colab used to run inference as before #####
+
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path= MODELS_FOLDER_PATH + "/autoencoder_model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Test model on random input data.
+input_shape = input_details[0]['shape']
+test_dataa = tf.expand_dims(tf.convert_to_tensor(input_data_test, dtype=tf.float32), 0)
+
+input_data = test_dataa
+
+interpreter.set_tensor(input_details[0]['index'], input_data)
 
 
+interpreter.invoke()
+
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print("MODEL PREDICTIONS: ", output_data)
+
+#find MAE
+mae = np.mean(np.abs(input_data_test - output_data))
+print("MEAN ABSOLUTE ERROR: ", mae)
 
 
 
