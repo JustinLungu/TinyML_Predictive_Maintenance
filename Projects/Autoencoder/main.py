fe371eaf0f8074fe753f81d64f4c9a346fadb0b1
JustinLungu <iustinl113@gmail.com>
2024-05-10 12:02:40 +0200

    75hz model trained

--- Projects/Autoencoder/main.py
+++ Projects/Autoencoder/main.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
     random_seed = 42
     np.random.seed(random_seed)
 
-    normal_data = Data(capture = "4", hertz = "60", volume = "30")
+    normal_data = Data(capture = "4", hertz = "75", volume = "50")
     abnormal_data = Data(capture = "2", hertz = "40", volume = "60")
 
     #normal_data = Data(capture = "2", hertz = "40", volume = "60")
