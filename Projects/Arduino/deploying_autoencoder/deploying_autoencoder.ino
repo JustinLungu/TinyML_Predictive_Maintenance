739ab5bbf5b117df15d116cdd850fd4f5e3db29a
diana-catana <99015379+diana-catana@users.noreply.github.com>
2024-07-30 16:18:46 +0300

    Deleted commented code

--- Projects/Arduino/deploying_autoencoder/deploying_autoencoder.ino
+++ Projects/Arduino/deploying_autoencoder/deploying_autoencoder.ino
@@ -2,7 +2,6 @@
 #include <TensorFlowLite.h>
 #include <Arduino_LSM9DS1.h>
 
-//#include <cstdlib>
 
 #include "tensorflow/lite/micro/all_ops_resolver.h"// register TF Lite operations
 #include "tensorflow/lite/micro/micro_interpreter.h"// runs the inference engine
@@ -10,10 +9,8 @@
 #include "tensorflow/lite/micro/system_setup.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
-//ALREADY HAVE IT: #include "TensorFlowLite.h"
 //#include "tensorflow/lite/micro/kernels/micro_ops.h"
 //NOT EXISTENT ANYMORE: #include "tensorflow/lite/micro/micro_error_reporter.h"
-//ALREADY HAVE IT: #include "tensorflow/lite/micro/micro_interpreter.h"
 //#include "tensorflow/lite/micro/micro_mutable_op_resolver.h" //theoretically same thing as the all_ops_resolver.h
 //NOT EXISTENT ANYMORE: #include "tensorflow/lite/version.h"
 
@@ -93,8 +90,8 @@ void setup() {
     return;
   }
 
-  pinMode(LEDR, OUTPUT); //LEDR
-  pinMode(LEDR, OUTPUT); // LEDR
+  pinMode(LEDR, OUTPUT); 
+  pinMode(LEDR, OUTPUT); 
 
   input = interpreter->input(0);
   output = interpreter->output(0);
@@ -102,53 +99,23 @@ void setup() {
 
 
 void loop() {
-  //3x1 input
-  //float inputData[INPUT_LENGTH] = {0.5075, 0.4825, 0.675};
 
   // used to read data from the accelerometer as input for the model  
   float inputBuffer[INPUT_LENGTH] = {0};
   float buffer[INPUT_LENGTH] = {0};
   int readingsTaken = 0;
 
-  // // Read 24 readings from accelerometer
-  // for(int i = 0; i < INPUT_LENGTH; i += 3){
-  //     if (IMU.accelerationAvailable()) {
-  //       IMU.readAcceleration(inputBuffer[i], inputBuffer[i + 1], inputBuffer[i + 2]);
-  //       Serial.println("set");
-  //     } else {
-  //       i -= 3;
-  //     }
-  // }
-  // Serial.println("Reading Window Done");
-
-
-
 
   // Read 24 readings from accelerometer
   for(int i = 0; i < 72; i += 3){
       if (IMU.accelerationAvailable()) {
         IMU.readAcceleration(buffer[i], buffer[i+1], buffer[i+2]);
-        // Serial.print(buffer[i]);
-        // Serial.print("   ");
-        // Serial.print(buffer[i+1]);
-        // Serial.print("   ");
-        // Serial.println(buffer[i+2]);
         readingsTaken++; // Increment the number of readings taken
       } else {
         i -= 3;
       }
   }
 
-
-  // Serial.println("THE INPUT WAS");
-
-  // for(int i = 0; i < INPUT_LENGTH; ++i){
-  //         Serial.print(buffer[i], 8);
-  //         Serial.print("   ");
-  // }
-  // Serial.println(" ");
-
-  // float buffer[INPUT_LENGTH] = {0.0};
   float mae = 0.0;
   float diff = 0.0;
   float sumAbsDiff = 0.0;
@@ -156,17 +123,8 @@ void loop() {
 
   
   for (int i = 0; i < INPUT_LENGTH; ++i) {
-    input->data.f[i] = (buffer[i] + 2)/4; //inputData[i]; 
+    input->data.f[i] = (buffer[i] + 2)/4; 
   }
-
-  // Serial.println("THE INPUT Normalized WAS");
-
-  // for(int i = 0; i < INPUT_LENGTH; ++i){
-  //         Serial.print(input->data.f[i], 8);
-  //         Serial.print("   ");
-  // }
-  // Serial.println(" ");
-
 
   // Run inference, and report any error. Call the invoke function and check for errors
   TfLiteStatus invoke_status = interpreter->Invoke();
@@ -182,29 +140,6 @@ void loop() {
   for (int i = 0; i < INPUT_LENGTH; ++i) {
       output_buffer[i] = output->data.f[i]; 
   }
-
-  // Serial.println("THE INPUT WAS");
-
-  // for(int i = 0; i < INPUT_LENGTH; ++i){
-  //         Serial.print(inputBuffer[i], 8);
-  //         Serial.print("   ");
-  // }
-  // Serial.println(" ");
-
-  // Serial.println("THE INPUT NORMALIZED WAS");
-
-  // for(int i = 0; i < INPUT_LENGTH; ++i){
-  //         Serial.print((inputBuffer[i] + 2)/4, 8);
-  //         Serial.print("   ");
-  // }
-  // Serial.println(" ");
-
-
-  // Serial.println("THE OUTPUT WAS");
-  // for(int i = 0; i < INPUT_LENGTH; ++i){
-  //         Serial.print(output_buffer[i], 8);
-  //         Serial.print("   ");
-  // }
 
   // calculate the mean absolute error
   sumAbsDiff = 0.0;
